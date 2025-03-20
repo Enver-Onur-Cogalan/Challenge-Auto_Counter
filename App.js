8cb0d1ee2ff8d7c05c125c1690f289bd76d1ea3f
Enver Onur Çoğalan <ericmoren@Enver-MacBook-Air.local>
2025-03-20 10:43:23 +0300

    :heavy_plus_sign: challenge finish

--- App.js
+++ App.js
@@ -2,27 +2,32 @@ import { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 const Timer = () => {
+  // Counter for state
   const [seconds, setSeconds] = useState(0);
   const [isFashing, setIsFlashing] = useState(false);
+  // State to control the blinking effect
   const [backgroundColor, setBackgroundColor] = useState('white');
 
+  // useEffect which increments the counter value every second
   useEffect(() => {
     const interval = setInterval(() => {
       setSeconds((prev) => prev + 1);
     }, 1000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(interval); // If the component is unmounted, the interval is cleared
   }, []);
 
+  // useEffect to make the screen flash red every 30 seconds
   useEffect(() => {
     if (seconds > 0 && seconds % 30 === 0) {
       setIsFlashing(true);
-      let flashCount = 0;
+      let flashCount = 0; // Keeps track of the number of blinks
 
       const flashInterval = setInterval(() => {
         setBackgroundColor((prev) => (prev === 'white' ? 'red' : 'white'));
         flashCount++;
 
+        // Create a flashing effect by changing the background at 200ms intervals
         if (flashCount >= 8) {
           clearInterval(flashInterval);
           setBackgroundColor('white');
@@ -31,7 +36,7 @@ const Timer = () => {
       }, 200);
 
     }
-  }, [seconds]);
+  }, [seconds]); // This effect is controlled as seconds change
 
   return (
     <View style={[styles.container, { backgroundColor }]}>
@@ -40,6 +45,7 @@ const Timer = () => {
   );
 };
 
+// Style definitions
 const styles = StyleSheet.create({
   container: {
     flex: 1,
